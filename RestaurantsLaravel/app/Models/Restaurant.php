6bec0c2aa2ccc39916ba7f504906c312b45861ca
Edouard de Romree de Vichenet <deromree.edouard@gmail.com>
2023-02-24 10:46:51 +0100

    exercices add to db

--- RestaurantsLaravel/app/Models/Restaurant.php
+++ RestaurantsLaravel/app/Models/Restaurant.php
@@ -8,5 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Restaurant extends Model
 {
     protected $table = 'restaurants'; 
+    protected $fillable = ['name','address','zipCode','town','country','description','review'] ;
+
     use HasFactory;
 }
