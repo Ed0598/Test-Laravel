749d66e40a79d367cdec3f05c0a93598510b9226
Edouard de Romrée <deromree.edouard@gmail.com>
2023-02-27 15:24:40 +0100

    suite tailwind

--- RestaurantsLaravel/resources/views/restaurants/create.blade.php
+++ RestaurantsLaravel/resources/views/restaurants/create.blade.php
@@ -8,60 +8,63 @@
     @vite('resources/css/app.css')
 </head>
 <body class="bg-gray-100">
-    <h1 class="text-3xl font-bold underline text-center mb-8">Create your restaurant</h1>
+    <h1 class="text-3xl font-bold underline text-center mb-8 mt-8">Create your restaurant</h1>
     <form action="/restaurants/create" method="post" class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden p-6">
         @csrf
         <div class="mb-4">
             <label for="name" class="block text-gray-700 font-bold mb-2 text-center">Name </label>
-            <input type="text" name='name' id='name' class="border border-gray-300 p-2 w-full rounded-md" required>
+            <input type="text" name='name' id='name' class="border border-gray-300 p-2 w-full rounded-md text-center">
             @error('name')
-                <div class="text-red-500 text-sm mt-1">{{ $message }}</div>
+                <div class="text-red-500 text-sm mt-1 text-center font-bold">{{ $message }}</div>
             @enderror
         </div>
         <div class="mb-4">
             <label for="address" class="block text-gray-700 font-bold mb-2 text-center">Address </label>
-            <input type="text" name='address' id='address' class="border border-gray-300 p-2 w-full rounded-md" required>
+            <input type="text" name='address' id='address' class="border border-gray-300 p-2 w-full rounded-md text-center">
             @error('address')
-                <div class="text-red-500 text-sm mt-1">{{ $message }}</div>
+                <div class="text-red-500 text-sm mt-1 text-center font-bold">{{ $message }}</div>
             @enderror
         </div>
         <div class="mb-4">
             <label for="zipCode" class="block text-gray-700 font-bold mb-2 text-center">Zip Code </label>
-            <input type="text" name='zipCode' id='zipCode' class="border border-gray-300 p-2 w-full rounded-md" required>
+            <input type="text" name='zipCode' id='zipCode' class="border border-gray-300 p-2 w-full rounded-md text-center" >
             @error('zipCode')
-                <div class="text-red-500 text-sm mt-1">{{ $message }}</div>
+                <div class="text-red-500 text-sm mt-1 text-center font-bold">{{ $message }}</div>
             @enderror
         </div>
         <div class="mb-4">
             <label for="town" class="block text-gray-700 font-bold mb-2 text-center">Town </label>
-            <input type="text" name='town' id='town' class="border border-gray-300 p-2 w-full rounded-md" required>
+            <input type="text" name='town' id='town' class="border border-gray-300 p-2 w-full rounded-md text-center" >
             @error('town')
-                <div class="text-red-500 text-sm mt-1">{{ $message }}</div>
+                <div class="text-red-500 text-sm mt-1 text-center font-bold">{{ $message }}</div>
             @enderror
         </div>
        
         <div class="mb-4">
             <label for="country" class="block text-gray-700 font-bold mb-2 text-center">Country </label>
-            <input type="text" name="country" id="country" class="border border-gray-300 p-2 w-full rounded-md">
+            <input type="text" name="country" id="country" class="border border-gray-300 p-2 w-full rounded-md text-center">
             @error('country')
-            <div class="text-red-500 text-sm mt-1">{{ $message }}</div>
+            <div class="text-red-500 text-sm mt-1 text-center font-bold">{{ $message }}</div>
             @enderror
         </div>
 
         <div class="mb-4">
             <label for="description" class="block text-gray-700 font-bold mb-2 text-center">Description </label>
-            <input type="text" name="description" id="description" class="border border-gray-300 p-2 w-full rounded-md">
+            <textarea name="description" id="description" class="border border-gray-300 p-2 w-full rounded-md h-40"></textarea>
             @error('description')
-            <div class="text-red-500 text-sm mt-1">{{ $message }}</div>
+            <div class="text-red-500 text-sm mt-1 text-center font-bold">{{ $message }}</div>
             @enderror
         </div>
         <label for="review" class="block text-gray-700 font-bold mb-2 text-center">Review </label>
-        <input type="text" name='review' id='review' class = "border border-gray-300 p-2 w-full rounded-md">
+        <input type="number" max ="10" min="0" name='review' id='review' class = "border border-gray-300 p-2 w-full rounded-md">
         @error('review')
-            <div class="alert alert-danger">{{ $message }}</div>
+            <div class="text-red-500 text-sm mt-1 text-center font-bold">{{ $message }}</div>
         @enderror
-        <input type="submit" value="Submit"></input>
+    <div class ="flex flex-row justify-center">
+        <input type="submit" value="Submit" class ="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-8 " ></input>
+    </div>
     </form>
+</div>
     
 </body>
 </html>
--- RestaurantsLaravel/resources/views/restaurants/index.blade.php
+++ RestaurantsLaravel/resources/views/restaurants/index.blade.php
@@ -1,14 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Restaurants Laravel</title>
+    @vite('resources/css/app.css')
+</head>
+<body class="bg-gray-100">
+    <h1 class="text-3xl font-bold underline text-center mb-8 mt-8">Welcome on our Laravel list of restaurants</h1>
+    <div class="flex flex-col items-center">
+    @foreach ($restaurants as $restaurant)
+        <div class="flex justify-center bg-white rounded-lg shadow-md p-4 mb-4 w-full md:w-2/3 lg:w-1/2 xl:w-1/3">
+            <div class="w-full flex flex-col items-center">
+                <h2 class="text-xl font-semibold mb-2 text-center">{{ $restaurant->name }}</h2>
+                <p class="text-gray-500 text-sm mb-1">{{ $restaurant->address }}</p>
+                <p class="text-gray-500 text-sm mb-1">{{ $restaurant->zipCode }} {{ $restaurant->town }}</p>
+                <p class="text-gray-500 text-sm mb-2">{{ $restaurant->country }}</p>
+                <p class="text-gray-700 mb-4 text-center">{{ $restaurant->description }}</p>
+            </div>
+            <button><a href="/restaurants/edit/{restaurant}">Edit</a></button>
+        </div>
+    @endforeach
+    </div>
+    <div class="flex justify-center">
+        <form action="/restaurants/create" method="get">
+            <input type="submit" name="add" id="add" value="Ajouter un restaurant" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-8 mb-8">
+        </form>
+    </div>
+</body>
 
+</body>
 
-@foreach ($restaurants as $restaurant)
-    <p> {{ $restaurant->name }} </p>
-    <p> {{ $restaurant->address }} </p>
-    <p> {{ $restaurant->zipCode }} </p>
-    <p> {{ $restaurant->town }} </p>
-    <p> {{ $restaurant->country }} </p>
-    <p> {{ $restaurant->description }} </p>
-    <p> {{ $restaurant->review }} </p>
-    <p> {{ $restaurant->created_at }} </p>
-    <p> {{ $restaurant->updated_at }} </p>
-@endforeach
+</body>
+</html>
 
